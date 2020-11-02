81e0e76c17e393c5ea7b5101fda442614ea801c1
Bharat B <bbharat95@gmail.com>
2020-11-02 15:17:38 +0530

    Rearranged Code

--- src/Bitninja/Api.php
+++ src/Bitninja/Api.php
@@ -1,22 +1,21 @@
 <?php
 
-namespace Bitninja;
 
-class Api
+class BitninjaAPI
 {
 
     /**
-     * @var string 
+     * @var string
      */
     public $api_url = "https://reseller-api.bitninja.io/v1/";
 
     /**
-     * @var string 
+     * @var string
      */
     protected $api_key;
 
     /**
-     * @var string 
+     * @var string
      */
     protected $email;
 
@@ -44,16 +43,8 @@ class Api
     /**
      * @throws Exception
      */
-    public function validate(){
-        if(empty($this->email) || empty($this->api_key)){
-            throw new \Exception("Email or API Key is missing!");
-        }
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function authenticate(){
+    public function authenticate()
+    {
         $this->validate();
         $curl = new GuzzleHttp\Client([
             'base_uri' => "https://reseller-api.bitninja.io/oauth/",
@@ -61,12 +52,12 @@ class Api
             'verify' => false,
             'exceptions' => false
         ]);
-        $response = $curl->post('access_token',['form_params' => [
+        $response = $curl->post('access_token', ['form_params' => [
             'grant_type' => 'client_credentials',
             'client_id' => $this->email,
             'client_secret' => $this->api_key
         ]]);
-        if($response->getStatusCode() == 200){
+        if ($response->getStatusCode() == 200) {
             $output = json_decode($response->getBody());
             $this->access_token = $output->access_token;
             $this->connection = new GuzzleHttp\Client([
@@ -80,27 +71,29 @@ class Api
                 'exceptions' => false
             ]);
         } else {
-            throw new \Exception($response->getBody());
+            throw new Exception($response->getBody());
         }
     }
 
     /**
-     * @param $endpoint
-     * @param array $params
-     * @param string $method
+     * @throws Exception
+     */
+    public function validate()
+    {
+        if (empty($this->email) || empty($this->api_key)) {
+            throw new Exception("Email or API Key is missing!");
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $email
      * @return mixed
      * @throws Exception
      */
-    public function call($endpoint, $params = [], $method = 'post'){
-        if(empty($this->access_token)){
-            throw new \Exception("Please authenticate first!");
-        }
-        $response = $this->connection->$method($endpoint, $params);
-        $output = json_decode($response->getBody());
-        if($output->code != 200){
-            throw new \Exception($output->message.implode("\n",$output->fields));
-        }
-        return json_decode($response->getBody());
+    public function registerUser($name, $email)
+    {
+        return $this->call('users', ['form_params' => ['body' => json_encode(['name' => $name, 'email' => $email])]]);
     }
 
 
@@ -109,13 +102,23 @@ class Api
      */
 
     /**
-     * @param $name
-     * @param $email
+     * @param $endpoint
+     * @param array $params
+     * @param string $method
      * @return mixed
      * @throws Exception
      */
-    public function registerUser($name, $email){
-        return $this->call('users',['form_params' => ['body' => json_encode(['name' => $name, 'email' => $email]) ] ]);
+    public function call($endpoint, $params = [], $method = 'post')
+    {
+        if (empty($this->access_token)) {
+            throw new Exception("Please authenticate first!");
+        }
+        $response = $this->connection->$method($endpoint, $params);
+        $output = json_decode($response->getBody());
+        if ($output->code != 200) {
+            throw new Exception($output->message . implode("\n", $output->fields));
+        }
+        return json_decode($response->getBody());
     }
 
     /**
@@ -123,35 +126,38 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function getInstructions($user_id){
-        $response = $this->call("users/{$user_id}/install",[],'get');
+    public function getInstructions($user_id)
+    {
+        $response = $this->call("users/{$user_id}/install", [], 'get');
         return $response->message->instructions;
 
     }
 
     /**
-     * @return mixed
-     * @throws Exception
-     */
-    public function listAllUsers(){
-        $response = $this->call("users",[],'get');
-        return $response->message;
-    }
-
-    /**
      * @param $user_id
      * @return mixed
      * @throws Exception
      */
-    public function getUserById($user_id){
+    public function getUserById($user_id)
+    {
         $users = $this->listAllUsers();
-        if(count($users) > 0){
-            foreach ($users as $user){
-                if($user->id == $user_id){
+        if (count($users) > 0) {
+            foreach ($users as $user) {
+                if ($user->id == $user_id) {
                     return $user;
                 }
             }
         }
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function listAllUsers()
+    {
+        $response = $this->call("users", [], 'get');
+        return $response->message;
     }
 
     /**
@@ -159,11 +165,12 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function getUserByEmail($email){
+    public function getUserByEmail($email)
+    {
         $users = $this->listAllUsers();
-        if(count($users) > 0){
-            foreach ($users as $user){
-                if($user->email == $email){
+        if (count($users) > 0) {
+            foreach ($users as $user) {
+                if ($user->email == $email) {
                     return $user;
                 }
             }
@@ -175,8 +182,9 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function detachUser($user_id){
-        return $this->call("users/{$user_id}",['form_params' => ['user_id' => $user_id ]],'delete');
+    public function detachUser($user_id)
+    {
+        return $this->call("users/{$user_id}", ['form_params' => ['user_id' => $user_id]], 'delete');
     }
 
     /**
@@ -187,8 +195,9 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function listAllServers(){
-        $response = $this->call("servers",[],'get');
+    public function listAllServers()
+    {
+        $response = $this->call("servers", [], 'get');
         return $response->message;
     }
 
@@ -197,8 +206,9 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function listUsersServers($user_id){
-        $response = $this->call("users/{$user_id}/servers",[],'get');
+    public function listUsersServers($user_id)
+    {
+        $response = $this->call("users/{$user_id}/servers", [], 'get');
         return $response->message;
     }
 
@@ -207,23 +217,16 @@ class Api
      * API Methods License Keys
      */
 
+
     /**
      * @param $user_id
      * @param $ip
      * @return mixed
      * @throws Exception
      */
-    public function createLicenseKey($user_id, $ip){
-        return $this->call("licensekeys",['form_params' => ['body' => json_encode(['user_id' => $user_id, 'ip_adddress' => $ip])] ],'post');
-    }
-
-    /**
-     * @return mixed
-     * @throws Exception
-     */
-    public function listAllLicenseKeys(){
-        $response = $this->call("licensekeys",[],'get');
-        return $response->message;
+    public function createLicenseKey($user_id, $ip)
+    {
+        return $this->call("licensekeys", ['form_params' => ['body' => json_encode(['user_id' => $user_id, 'ip_adddress' => $ip])]], 'post');
     }
 
     /**
@@ -231,13 +234,24 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function getLicenseKey($server_ip){
+    public function getLicenseKey($server_ip)
+    {
         $license_keys = $this->listAllLicenseKeys();
-        foreach($license_keys as $key) {
-            if($key->server_ip == $server_ip){
+        foreach ($license_keys as $key) {
+            if ($key->server_ip == $server_ip) {
                 return $key;
             }
         }
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function listAllLicenseKeys()
+    {
+        $response = $this->call("licensekeys", [], 'get');
+        return $response->message;
     }
 
     /**
@@ -245,8 +259,9 @@ class Api
      * @return mixed
      * @throws Exception
      */
-    public function terminateLicenseKey($license_key){
-        return $this->call("licensekeys/{$license_key}",['form_params' => ['license_key' => $license_key]],'delete');
+    public function terminateLicenseKey($license_key)
+    {
+        return $this->call("licensekeys/{$license_key}", ['form_params' => ['license_key' => $license_key]], 'delete');
     }
 
     /**
@@ -259,8 +274,9 @@ class Api
      * @return object
      * @throws Exception
      */
-    public function attachLicense($user_id, $ip){
-        return $this->call("licenses",['form_params' => ['body' => json_encode(['server_id' => $user_id])] ],'post');
+    public function attachLicense($user_id, $ip)
+    {
+        return $this->call("licenses", ['form_params' => ['body' => json_encode(['server_id' => $user_id])]], 'post');
     }
 
     /**
@@ -268,16 +284,18 @@ class Api
      * @return object
      * @throws Exception
      */
-    public function dettachLicense($server_id){
-        return $this->call("licenses/{$server_id}",[],'get');
+    public function dettachLicense($server_id)
+    {
+        return $this->call("licenses/{$server_id}", [], 'get');
     }
 
     /**
      * @return mixed
      * @throws Exception
      */
-    public function listAllLicense(){
-        $response = $this->call("licenses",[],'get');
+    public function listAllLicense()
+    {
+        $response = $this->call("licenses", [], 'get');
         return $response->message;
     }
 
